f7c090679472cba2c7a722dbe45e362549426f2a
Gavin Gray <gavin@vmware.com>
2012-10-11 12:07:57 -0400

    First failing test.

--- spring-mvc-annotation-processor-test/src/test/java/com/thegrayfiles/tests/acceptance/SimpleControllerTests.java
+++ spring-mvc-annotation-processor-test/src/test/java/com/thegrayfiles/tests/acceptance/SimpleControllerTests.java
@@ -1,12 +1,17 @@
 package com.thegrayfiles.tests.acceptance;
 
+import org.springframework.web.client.RestTemplate;
 import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
 
 public class SimpleControllerTests {
 
+    private RestTemplate restTemplate = new RestTemplate();
+
     @Test
     public void canGetResourceFromController() {
-
+        String response = restTemplate.getForEntity("http://localhost:8080/test", String.class).getBody();
+        assertTrue(response.length() != 0, "Response string length is zero!");
     }
-
 }
