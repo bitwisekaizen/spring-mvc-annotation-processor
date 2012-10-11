0baf85defce65b2ebeb855ee79355605dd796cee
Gavin Gray <gavin@vmware.com>
2012-10-11 12:04:26 -0400

    First test of test controller.

---
+++ spring-mvc-annotation-processor-test/src/test/java/com/thegrayfiles/tests/acceptance/SimpleControllerTests.java
@@ -0,0 +1,12 @@
+package com.thegrayfiles.tests.acceptance;
+
+import org.testng.annotations.Test;
+
+public class SimpleControllerTests {
+
+    @Test
+    public void canGetResourceFromController() {
+
+    }
+
+}
