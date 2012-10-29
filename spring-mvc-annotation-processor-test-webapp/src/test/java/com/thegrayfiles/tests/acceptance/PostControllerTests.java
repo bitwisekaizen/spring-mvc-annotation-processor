342c9d4018b44756257d334bf759b178e9c13001
Gavin Gray <gavin@vmware.com>
2012-10-28 20:23:00 -0400

    Failing test for posting with request body.

--- spring-mvc-annotation-processor-test-webapp/src/test/java/com/thegrayfiles/tests/acceptance/PostControllerTests.java
+++ spring-mvc-annotation-processor-test-webapp/src/test/java/com/thegrayfiles/tests/acceptance/PostControllerTests.java
@@ -1,9 +1,13 @@
 package com.thegrayfiles.tests.acceptance;
 
 import com.thegrayfiles.builders.DynamicallyGeneratedClientMethodInvoker;
+import com.thegrayfiles.marshallable.TestEntity;
 import org.testng.annotations.Test;
 
+import java.util.UUID;
+
 import static com.thegrayfiles.builders.DynamicallyGeneratedClientMethodInvoker.aMethodNamed;
+import static org.testng.Assert.assertEquals;
 
 public class PostControllerTests {
 
@@ -12,5 +16,14 @@ public class PostControllerTests {
         DynamicallyGeneratedClientMethodInvoker invoker = aMethodNamed("canPostWithoutParametersOrReturnValue");
         invoker.invoke();
         // nothing to assert at the moment besides no exception thrown
+    }
+
+    @Test
+    public void canPostWithRequestBody() {
+        String testEntityName = UUID.randomUUID().toString();
+        TestEntity testEntityToPost = new TestEntity(testEntityName);
+        DynamicallyGeneratedClientMethodInvoker postInvoker = aMethodNamed("canPostWithRequestBody").withArgument(TestEntity.class, testEntityToPost);
+        TestEntity postedTestEntity = postInvoker.invoke(TestEntity.class);
+        assertEquals(postedTestEntity.getName(), testEntityName);
     }
 }
